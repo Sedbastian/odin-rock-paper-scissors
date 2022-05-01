37af880111c5dfce357ce3909e7f96970fc80a24
Sedbastian <SebastianIgnacioLuciani@gmail.com>
2022-05-01 13:51:45 -0300

    Terminar juego. Traducir al español

--- javascript.js
+++ javascript.js
@@ -2,13 +2,13 @@ function computerPlay() {
     let Play;
     switch(Math.floor(Math.random() * 3 + 1)) {
         case 1:
-            Play = "Rock";
+            Play = "piedra";
             break;
         case 2:
-            Play = "Paper";
+            Play = "papel";
             break;
         case 3:
-            Play = "Scissors";
+            Play = "tijera";
             break;
     }
     return Play;
@@ -19,43 +19,62 @@ function singleRound(playerSelection, computerSelection) {
     let playerSelectionLowerCase = playerSelection.toLowerCase();
     let computerSelectionLowerCase = computerSelection.toLowerCase();
 
-    if (playerSelectionLowerCase === "rock" && computerSelectionLowerCase === "rock") {
-        return "Draw";
+    if (playerSelectionLowerCase === "piedra" && computerSelectionLowerCase === "piedra") {
+        console.log("Empate");
+        return 0;
     }
-    if (playerSelectionLowerCase === "rock" && computerSelectionLowerCase === "paper") {
-        return "You lose";
+    if (playerSelectionLowerCase === "piedra" && computerSelectionLowerCase === "papel") {
+        console.log("Perdiste esta vuelta");
+        return -1;
     }
-    if (playerSelectionLowerCase === "rock" && computerSelectionLowerCase === "scissors") {
-        return "You win";
+    if (playerSelectionLowerCase === "piedra" && computerSelectionLowerCase === "tijera") {
+        console.log("Ganaste esta vuelta");
+        return 1;
     }
-    if (playerSelectionLowerCase === "paper" && computerSelectionLowerCase === "rock") {
-        return "You win";
+    if (playerSelectionLowerCase === "papel" && computerSelectionLowerCase === "piedra") {
+        console.log("Ganaste esta vuelta");
+        return 1;
     }
-    if (playerSelectionLowerCase === "paper" && computerSelectionLowerCase === "paper") {
-        return "Draw";
+    if (playerSelectionLowerCase === "papel" && computerSelectionLowerCase === "papel") {
+        console.log("Empate");
+        return 0;
     }
-    if (playerSelectionLowerCase === "paper" && computerSelectionLowerCase === "scissors") {
-        return "You lose";
+    if (playerSelectionLowerCase === "papel" && computerSelectionLowerCase === "tijera") {
+        console.log("Perdiste esta vuelta");
+        return -1;
     }
-    if (playerSelectionLowerCase === "scissors" && computerSelectionLowerCase === "rock") {
-        return "You lose";
+    if (playerSelectionLowerCase === "tijera" && computerSelectionLowerCase === "piedra") {
+        console.log("Perdiste esta vuelta");
+        return -1;
     }
-    if (playerSelectionLowerCase === "scissors" && computerSelectionLowerCase === "paper") {
-        return "You win";
+    if (playerSelectionLowerCase === "tijera" && computerSelectionLowerCase === "papel") {
+        console.log("Ganaste esta vuelta");
+        return 1;
     }
-    if (playerSelectionLowerCase === "scissors" && computerSelectionLowerCase === "scissors") {
-        return "Draw";
+    if (playerSelectionLowerCase === "tijera" && computerSelectionLowerCase === "tijera") {
+        console.log("Empate");
+        return 0;
     }
 }
 
 function game() {
+    let score = 0;
     for (let i = 0; i < 5; i++) {
-        let playerChoice = prompt("Your move:");
-        console.log(`Round ${i+1}:`);
+        let playerChoice = prompt("Piedra, papel o tijera:");
+        console.log(`Vuelta ${i+1}:`);
         let computerMove = computerPlay();
-        console.log(`Computer plays ${computerMove}`);
-        console.log(singleRound(playerChoice, computerMove));
-
+        console.log(`La compu juega: ${computerMove}`);
+        score = score + singleRound(playerChoice, computerMove);
+        console.log(`Tus puntos de la partida: ${score}`);
+    }
+    if (score < 0) {
+        console.log("Perdiste la partida");
+    }
+    if (score === 0) {
+        console.log("Partida empatada");
+    }
+    if (score > 0) {
+        console.log("Ganaste la partida");
     }
 }
 
